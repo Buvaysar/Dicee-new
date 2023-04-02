c3606bf05bcd3531cc4d5e176fd9647711342cba
Mayrbek <buvaysar1988@mail.ru>
2023-04-02 03:53:21 +0300

    Initial Commit

---
+++ Dicee-newUITests/Dicee_newUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Dicee_newUITestsLaunchTests.swift
+//  Dicee-newUITests
+//
+//  Created by Magomadov on 02.04.2023.
+//
+
+import XCTest
+
+final class Dicee_newUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
